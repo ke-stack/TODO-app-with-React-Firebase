3265fcdb88720f5fc12d780214f609c8874b89d3
ke-stack <kingkevo254@gmail.com>
2022-08-23 04:24:25 +0300

    commit

---
+++ todo_app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <input />
+      <button>Add Todo</button>
+      
+      <ul>
+        <li> Take dogs out</li>
+        <li> Write down notes</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
